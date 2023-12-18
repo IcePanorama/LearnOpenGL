63c958dc0f3d4a2c4f5ac7653b6c18f5c7f5bc19
IcePanorama <thriftedroot@protonmail.com>
2023-12-18 14:39:18 -0500

    now displaying 4 smiley faces (Textures/Exercise 2a)

--- src/main.cpp
+++ src/main.cpp
@@ -41,10 +41,10 @@ int main()
 
     float vertices[] = {
         // positions        //colors            // Texture coords
-        0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,     // top right
-        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,     // bottom right 
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f,     // bottom left
-        -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f      // bottom left
+        0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,     // top right
+        0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   2.0f, 0.0f,     // bottom right 
+       -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,     // bottom left
+       -0.5f, 0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f      // bottom left
     };
     unsigned int indices[] = {
         0, 1, 3,    // first tri
