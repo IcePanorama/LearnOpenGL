6fd0d3d30b68891633f973096f00b45919b274e9
IcePanorama <thriftedroot@protonmail.com>
2024-01-10 10:43:12 -0500

    now applying rotation over time and translation to crate

--- src/main.cpp
+++ src/main.cpp
@@ -94,17 +94,18 @@ int main()
     ourShader.setInt("texture2", 1);
     float shaderMix = 0.0f;
 
-    glm::mat4 trans = glm::mat4(1.0);
-    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-
-    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
     while (!glfwWindowShouldClose(window))
     {
         // Input
         processInput(window);
+
+        glm::mat4 trans = glm::mat4(1.0);
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f,
+                                                                   1.0f));
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         // render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
