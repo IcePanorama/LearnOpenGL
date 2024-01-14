379b931eff59229c114e32bdc908509d1e540c80
IcePanorama <thriftedroot@protonmail.com>
2024-01-14 13:23:48 -0500

    now drawing 10 cubes on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -92,6 +92,18 @@ int main()
       -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
       -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
+    glm::vec3 cubePositions[] = {
+      glm::vec3( 0.0f, 0.0f, 0.0f),
+      glm::vec3( 2.0f, 5.0f,-15.0f),
+      glm::vec3(-1.5f,-2.2f,-2.5f),
+      glm::vec3(-3.8f,-2.0f,-12.3f),
+      glm::vec3( 2.4f,-0.4f,-3.5f),
+      glm::vec3(-1.7f, 3.0f,-7.5f),
+      glm::vec3( 1.3f,-2.0f,-2.5f),
+      glm::vec3( 1.5f, 2.0f,-2.5f),
+      glm::vec3( 1.5f, 0.2f,-1.5f),
+      glm::vec3(-1.3f, 1.0f,-1.5f)
+    };
 
     unsigned int VBO, VAO; //, EBO; 
     glGenVertexArrays(1, &VAO);
@@ -147,21 +159,21 @@ int main()
         //activate shader
         ourShader.use();
 
-        glm::mat4 model = glm::mat4(1.0f);
+        //glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
 
-        model = glm::rotate(model, (float)glfwGetTime(),
-                            glm::vec3(0.5f, 1.0f, 0.0f));
+        //model = glm::rotate(model, (float)glfwGetTime(),
+                            //glm::vec3(0.5f, 1.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f),
                                       (float)SCR_WIDTH / (float)SCR_HEIGHT,
                                       0.1f, 100.0f);
 
         // retrieve the matrix uniform locs
-        int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        //int modelLoc = glGetUniformLocation(ourShader.ID, "model");
         int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
 
         int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
@@ -173,7 +185,17 @@ int main()
 
         // render container
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+          glm::mat4 model = glm::mat4(1.0f);
+          model = glm::translate(model, cubePositions[i]);
+          float angle = 20.0f * i;
+          model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+          int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+          glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+          glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // Swap buffers and poll I/O events
         glfwSwapBuffers(window);
